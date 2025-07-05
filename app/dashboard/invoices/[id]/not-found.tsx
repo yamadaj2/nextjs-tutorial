10598782cbf8a9f60fe6805133afbbb949d5d0e4
jonathonyamada <jonathon.yamada@gmail.com>
2025-07-05 21:32:44 +0100

    added not found functionality

--- app/dashboard/invoices/[id]/not-found.tsx
+++ app/dashboard/invoices/[id]/not-found.tsx
@@ -12,6 +12,7 @@ export default function NotFound() {
         className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
       >
         Go Back
+    
       </Link>
     </main>
   );
